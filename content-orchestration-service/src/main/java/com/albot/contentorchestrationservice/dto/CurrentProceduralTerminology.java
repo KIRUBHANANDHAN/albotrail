e862976a70e4dba8b80e72bbefbae8d05504df0e
vamsikrishna <vamkrishna@blackberry.com>
2020-07-17 04:32:13 +0530

    Added End Points  For CurrentProceduralTerminology

---
+++ content-orchestration-service/src/main/java/com/albot/contentorchestrationservice/dto/CurrentProceduralTerminology.java
@@ -0,0 +1,32 @@
+package com.albot.contentorchestrationservice.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder(toBuilder = true)
+@EqualsAndHashCode
+public class CurrentProceduralTerminology {
+    @ApiModelProperty("Field is used for holding rowId data as a Integer type.")
+    private Integer rowId;
+    @ApiModelProperty("Field is used for holding category data as a Integer type.")
+    private Integer category;
+    @ApiModelProperty("Field is used for holding sectionRange data as a String type.")
+    private String sectionRange;
+    @ApiModelProperty("Field is used for holding sectionHeader data as a String type.")
+    private String sectionHeader;
+    @ApiModelProperty("Field is used for holding subSectionRange data as a String type.")
+    private String subSectionRange;
+    @ApiModelProperty("Field is used for holding subSectionHeader data as a String type.")
+    private String subSectionHeader;
+    @ApiModelProperty("Field is used for holding codeSuffix data as a String type.")
+    private String codeSuffix;
+    @ApiModelProperty("Field is used for holding minCodeInSubSection data as a Integer type.")
+    private Integer minCodeInSubSection;
+    @ApiModelProperty("Field is used for holding maxCodeInSubSection data as a Integer type.")
+    private Integer maxCodeInSubSection;
+}
