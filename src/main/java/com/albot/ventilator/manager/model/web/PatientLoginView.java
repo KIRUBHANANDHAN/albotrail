b8cb5b4cb3e8ccd8ebd69516c3ec4d9f9ee7e8be
pratiks360 <pratiks360@gmail.com>
2020-11-25 10:25:23 +0530

    additional fields added to user patient details

--- src/main/java/com/albot/ventilator/manager/model/web/PatientLoginView.java
+++ src/main/java/com/albot/ventilator/manager/model/web/PatientLoginView.java
@@ -12,4 +12,7 @@ import lombok.*;
 public class PatientLoginView {
     @ApiModelProperty(value = "Field is used for holding patientId data as a Integer type.", required = true)
     private Integer patientId;
+    private String patientName;
+    private Integer age;
+    private String gender;
 }
