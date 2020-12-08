4cd98c4c917ece35a5115033ce1cd8615979c5fe
pratiks360 <pratiks360@gmail.com>
2020-12-08 13:14:12 +0530

    PatientController Autowiring fixed

--- src/main/java/io/albot/ventilator/manager/controllers/PatientController.java
+++ src/main/java/io/albot/ventilator/manager/controllers/PatientController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PatientController {
 
     private static final Logger logger = LoggerFactory.getLogger(PatientController.class);
-
+    @Autowired
     private PatientService patientService;
 
 
